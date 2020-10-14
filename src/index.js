741d4977fb644ee285dbba0b5d65f2131d32f46c
Turbin Alex <turbin28@gmail.com>
2020-10-14 15:38:50 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+function cl(str) { console.log(str) }
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strLength = str.length / 2
+    do {
+        for (let x of bracketsConfig) {
+            str = str.replace(x[0] + x[1], '');
+        }
+        strLength--
+    } while (strLength > 0)
+    if (str.length === 0) { return true }
+    return false
 }
